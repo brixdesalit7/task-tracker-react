f5af4a8ddc1cae0a26b3792437817a48bc361b4c
brixdesalit7 <brixdesalit7@gmail.com>
2024-06-23 14:52:28 +0800

    Added toggle

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import AddTask from './components/AddTask'
 import TaskList from './components/TaskList'
 
 const App = () => {
+  const taskList = [
+    {
+      id: 1,
+      title: "Task 1",
+      status: "Finished"
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      status: "In Progress"
+    },
+    {
+      id: 3,
+      title: "Task 3",
+      status: "Finished"
+    },
+  ]
   return (
     <>
-    <div className="task-tracker">
-      <h1 className="task-tracker__heading">Task Tracker</h1>
-      <AddTask/>
-      <TaskList/>
-    </div>
-   </>
+      <div className="task-tracker">
+        <h1 className="task-tracker__heading">Task Tracker</h1>
+        <AddTask />
+        <TaskList task={taskList}/>
+      </div>
+    </>
   )
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,44 +1,61 @@
 import React from 'react'
+import { useState } from 'react'
 
-const TaskList = () => {
+const TaskList = ({ task }) => {
+    const [update, setUpdate] = useState({});
+    
+    function showUpdate(id) {
+        setUpdate(prevVal => ({
+            ...prevVal,
+            [id] : !prevVal[id]
+        }));
+    }
     return (
         <table className="task-tracker__table">
             <thead className="task-tracker__table__thead">
                 <tr className="task-tracker__table__thead__tr">
                     <th className="task-tracker__table__thead__tr__th">ID</th>
-                    <th className="task-tracker__table__thead__tr__th">Task Name</th>
+                    <th className="task-tracker__table__thead__tr__th title">Task Name</th>
                     <th className="task-tracker__table__thead__tr__th">Task Status</th>
                     <th className="task-tracker__table__thead__tr__th">Option</th>
                 </tr>
             </thead>
             <tbody className="task-tracker__table__tbody">
-                <tr className="task-tracker__table__tbody__tr">
-                    <th className="task-tracker__table__tbody__tr__th task-id">ID</th>
-                    <th className="task-tracker__table__tbody__tr__th task-name">Task Name Task NameTask NameTask NameTask NameTask NameTask NameTask Name</th>
-                    <th className="task-tracker__table__tbody__tr__th">Task Status</th>
-                    <th className="task-tracker__table__tbody__tr__th">
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-edit">Edit</button>
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-delete">Delete</button>
-                    </th>
-                </tr>
-                <tr className="task-tracker__table__tbody__tr">
-                    <th className="task-tracker__table__tbody__tr__th task-id">ID</th>
-                    <th className="task-tracker__table__tbody__tr__th task-name">Task Name Task NameTask NameTask NameTask NameTask NameTask NameTask Name</th>
-                    <th className="task-tracker__table__tbody__tr__th">Task Status</th>
-                    <th className="task-tracker__table__tbody__tr__th">
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-edit">Edit</button>
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-delete">Delete</button>
-                    </th>
-                </tr>
-                <tr className="task-tracker__table__tbody__tr">
-                    <th className="task-tracker__table__tbody__tr__th task-id">ID</th>
-                    <th className="task-tracker__table__tbody__tr__th task-name">Task Name Task NameTask NameTask NameTask NameTask NameTask NameTask Name</th>
-                    <th className="task-tracker__table__tbody__tr__th">Task Status</th>
-                    <th className="task-tracker__table__tbody__tr__th">
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-edit">Edit</button>
-                        <button className="task-tracker__table__tbody__tr__th__btn btn-delete">Delete</button>
-                    </th>
-                </tr>
+                {task.map((val) => {
+                    if (!update[val.id]) {
+                        return (
+                            <tr className="task-tracker__table__tbody__tr" key={val.id}>
+                                <th className="task-tracker__table__tbody__tr__th task-id">{val.id}</th>
+                                <th className="task-tracker__table__tbody__tr__th task-name">{val.title}</th>
+                                <th className="task-tracker__table__tbody__tr__th">{val.status}</th>
+                                <th className="task-tracker__table__tbody__tr__th">
+                                    <button className="task-tracker__table__tbody__tr__th__btn btn-edit" onClick={() => showUpdate(val.id)}>Edit</button>
+                                    <button className="task-tracker__table__tbody__tr__th__btn btn-delete">Delete</button>
+                                </th>
+                            </tr>
+                        )
+                    } else {
+                        return (
+                            <tr className="task-tracker__table__tbody__tr" key={val.id}>
+                                <th className="task-tracker__table__tbody__tr__th task-id">{val.id}</th>
+                                <th className="task-tracker__table__tbody__tr__th task-name">
+                                    <input type="text" className="task-tracker__table__tbody__tr__th__input-text" defaultValue={val.title} />
+                                </th>
+                                <th className="task-tracker__table__tbody__tr__th">
+                                    <select className="task-tracker__table__tbody__tr__th__select" defaultValue={val.status}>
+                                        <option value="finished">Finished</option>
+                                        <option value="inprogress">In Progress</option>
+                                    </select>
+                                </th>
+                                <th className="task-tracker__table__tbody__tr__th">
+                                    <button className="task-tracker__table__tbody__tr__th__btn btn-save">Save</button>
+                                    <button className="task-tracker__table__tbody__tr__th__btn btn-delete" onClick={() => showUpdate(val.id)}>Close</button>
+                                </th>
+                            </tr>
+                        )
+                    }
+                })}
+
             </tbody>
         </table>
     )
